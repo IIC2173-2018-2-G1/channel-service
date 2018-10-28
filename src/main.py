a7afaeef8b1c12a4200b75f1440e6291ba93a8eb
cirey1 <cirey@uc.cl>
2018-10-28 20:04:08 -0300

    Merge changes

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ class ChannelListAPI(Resource):
         MONGO.db.channels.insert_one(new_channel)
         _last_added = MONGO.db.channels.find().sort([("$natural", -1)]).limit(1)
         last_added = [channel for channel in _last_added]
-        return {'channels': [marshal(channel, channel_fields)
+        return {'channel': [marshal(channel, channel_fields)
                              for channel in last_added]}, 201
 
 
@@ -71,14 +71,10 @@ class ChannelAPI(Resource):
 
     def get(self, id):
         channel = MONGO.db.channels.find_one_or_404({"_id": id})
-        if len(channel) == 0:
-            abort(404)
         return {"channel": marshal(channel, channel_fields)}
 
     def put(self, id):
         channel = MONGO.db.channels.find_one_or_404({"_id": id})
-        if len(channel) == 0:
-            abort(404)
         args = self.reqparse.parse_args()
         MONGO.db.channels.update(
                                   { _id: "id" },
@@ -94,7 +90,7 @@ api = Api(app)
 
 # add api resources
 api.add_resource(ChannelListAPI, "/api/v1.0/channels", endpoint="channels")
-api.add_resource(ChannelAPI, "/api/v1.0/channels/<string:_id>", endpoint="<int:id>")
+api.add_resource(ChannelAPI, "/api/v1.0/channels/<string:_id>", endpoint="channel")
 
 if __name__ == "__main__":
     app.run(debug=True)
