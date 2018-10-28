61689f8a85fc0e6e6642aae1435e3351c6ef636c
cirey1 <cirey@uc.cl>
2018-10-28 18:18:25 -0300

    Avance1

--- src/main.py
+++ src/main.py
@@ -9,63 +9,86 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("DB")
 MONGO = PyMongo(app)
 
-hashtag_parser = reqparse.RequestParser()
-hashtag_parser.add_argument("hashtag", required=True)
+channel_parser = reqparse.RequestParser()
+channel_parser.add_argument("channel", required=True)
 
-hashtag_fields = {"_id": fields.String, "name": fields.String,
-                  "user_id": fields.Integer, "date": fields.DateTime}
+channel_fields = {"_id": fields.String, "name": fields.String,
+                  "description": fields.String, "users_sub": fields.List,
+                  "uri": fields.Url('channel'), "date": fields.DateTime}
 
 
-class HashtagListAPI(Resource):
+class ChannelListAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("name", type=str, required=True,
+                                   help="No channel name provided",
+                                   location="json")
+
+        self.reqparse.add_argument("description", type=str, required=True,
+                                   help="No channel description provided",
+                                   location="json")
+
+        super(ChannelListAPI, self).__init__()
+
+    def get(self):
+        _data = MONGO.db.channels.find()
+        channels_found = [channel for channel in _data]
+        return {'channels': [marshal(channel, channel_fields)
+                             for channel in channels_found]}
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        new_channel = {"name": args["name"], "description": args["description"],
+                   "date": datetime.now()}
+        MONGO.db.channels.insert_one(new_channel)
+        _last_added = MONGO.db.channels.find().sort([("$natural", -1)]).limit(1)
+        last_added = [channel for channel in _last_added]
+        return {'channels': [marshal(channel, channel_fields)
+                             for channel in last_added]}, 201
+
+
+class ChannelAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument(
             "name",
             type=str,
             required=True,
-            help="No hashtag name provided",
+            help="No channel name provided",
             location="json",
         )
-        self.reqparse.add_argument("description", type=str, default="",
-                                   location="json")
-        self.user_id = 1
-        super(HashtagListAPI, self).__init__()
+        self.reqparse.add_argument(
+            "description",
+            type=str,
+            required=True,
+            help="No channel description provided",
+            location="json",
+        )
 
-    def get(self):
-        _data = MONGO.db.hashtags.find()
-        hashtags = [hashtag for hashtag in _data]
-        return {'hashtags': [marshal(hashtag, hashtag_fields)
-                             for hashtag in hashtags]}
+        super(ChannelAPI, self).__init__()
 
-    def post(self):
+    def get(self, id):
+        channel = MONGO.db.channels.find_one_or_404({"_id": id})
+        if len(channel) == 0:
+            abort(404)
+        return {"channel": marshal(channel, channel_fields)}
+
+    def put(self, id):
+        channel = MONGO.db.channels.find_one_or_404({"_id": id})
+        if len(channel) == 0:
+            abort(404)
         args = self.reqparse.parse_args()
-        hashtag = {"name": args["name"], "user_id": self.user_id,
-                   "date": datetime.now()}
-        MONGO.db.hashtags.insert_one(hashtag)
-        _last_added = MONGO.db.hashtags.find().sort([("$natural", -1)]).limit(1)
-        last_added = [hashtag for hashtag in _last_added]
-        return {'hashtags': [marshal(hashtag, hashtag_fields)
-                             for hashtag in last_added]}, 201
-
-
-# class HashtagAPI(Resource):
-#     def __init__(self):
-#         self.reqparse = reqparse.RequestParser()
-#         self.reqparse.add_argument("name", type=str, location="json")
-#         super(HashtagAPI, self).__init__()
-
-#     def get(self, id):
-#         hashtag = MONGO.db.hashtags.find_one_or_404({"_id": id})
-#         if len(hashtag) == 0:
-#             abort(404)
-#         return {"hashtag": marshal(hashtag, hashtag_fields)}
+        for k, v in args.items():
+            if v is not None:
+                channel[k] = v
+        return {'channel': marshal(channel, channel_fields)}
 
 
 api = Api(app)
 
 # add api resources
-api.add_resource(HashtagListAPI, "/api/v1.0/hashtags", endpoint="hashtags")
-# api.add_resource(HashtagAPI, "/api/v1.0/hashtags/<int:id>", endpoint="hashtag")
+api.add_resource(ChannelListAPI, "/api/v1.0/channels", endpoint="channels")
+api.add_resource(ChannelAPI, "/api/v1.0/channels/<int:id>", endpoint="<int:id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
